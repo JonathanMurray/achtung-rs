01d6a4316a5255b2b3c0d4ab58a0462b216a14f4
Jonathan <jonte.murray@gmail.com>
2022-10-28 19:27:26 +0200

    Make simple Achtung controls

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use crossterm::Result;
+use anyhow::Result;
 
-use crossterm::cursor::{MoveTo, MoveToColumn, MoveToRow};
+use crossterm::cursor::{Hide, MoveTo, MoveToColumn, MoveToRow};
 use crossterm::event::Event::Key;
-use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::style::{Color, ResetColor, SetForegroundColor};
 use crossterm::terminal::{self, EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::QueueableCommand;
 use std::io::{self, Stdout, Write};
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use std::time::Duration;
+use std::{panic, thread};
 
 fn main() -> Result<()> {
+    panic::set_hook(Box::new(move |panic_info| {
+        let mut stdout = io::stdout();
+        restore_terminal(&mut stdout);
+        eprintln!("Panic: >{:?}<", panic_info);
+    }));
+
     let mut app = App::new()?;
     app.run()?;
 
@@ -16,57 +27,106 @@ fn main() -> Result<()> {
 
 struct App {
     stdout: Stdout,
+    unhandled_event: String,
+    elapsed_ticks: u32,
+    line: Vec<(u16, u16)>,
+    direction: (i32, i32),
 }
 
 impl App {
     pub fn new() -> Result<Self> {
-        terminal::enable_raw_mode()?;
         let mut stdout = io::stdout();
-        stdout.queue(EnterAlternateScreen)?;
-        stdout.flush()?;
-        Ok(Self { stdout })
+        claim_terminal(&mut stdout)?;
+        Ok(Self {
+            stdout,
+            unhandled_event: String::new(),
+            elapsed_ticks: 0,
+            line: vec![(2, 5)],
+            direction: (1, 0),
+        })
     }
 
     pub fn run(&mut self) -> Result<()> {
-        self.stdout.queue(EnterAlternateScreen)?;
-        self.stdout.flush()?;
-
-        self.draw_border()?;
-        self.stdout.flush()?;
-
-        let mut input = String::new();
+        let (sender, receiver) = mpsc::channel();
+        Self::spawn_periodic_timer(sender.clone());
+        Self::spawn_event_receiver(sender);
 
         loop {
-            input.clear();
-            let event = crossterm::event::read()?;
-
-            match event {
-                Key(KeyEvent {
-                    code: KeyCode::Char('c'),
-                    modifiers: KeyModifiers::CONTROL,
-                    kind: KeyEventKind::Press,
-                    state: _,
-                }) => break,
-                Key(KeyEvent {
-                    code: KeyCode::Char('q'),
-                    ..
-                }) => break,
-                _ => {}
-            }
-
             self.draw_border()?;
-            self.stdout.flush()?;
 
             self.stdout.queue(MoveToRow(1))?;
             self.stdout.queue(MoveToColumn(2))?;
             self.stdout
-                .write_all(format!("EVENT: {:?}", event).as_bytes())?;
+                .write_all(format!("Event: {:?}", self.unhandled_event).as_bytes())?;
+
+            self.stdout.queue(MoveToRow(2))?;
+            self.stdout.queue(MoveToColumn(2))?;
+            self.stdout
+                .write_all(format!("Elapsed: {:?}", self.elapsed_ticks).as_bytes())?;
+
+            self.stdout.queue(SetForegroundColor(Color::Red))?;
+            for point in &self.line {
+                self.stdout.queue(MoveTo(point.0, point.1))?;
+                self.stdout.write_all("X".as_bytes())?;
+            }
+            self.stdout.queue(ResetColor)?;
+
             self.stdout.flush()?;
+
+            match receiver.recv()? {
+                Message::Event(event) => match event {
+                    Key(KeyEvent {
+                        code: KeyCode::Char('c'),
+                        modifiers: KeyModifiers::CONTROL,
+                        kind: KeyEventKind::Press,
+                        state: _,
+                    }) => break,
+                    Key(KeyEvent {
+                        code: KeyCode::Char('q'),
+                        ..
+                    }) => break,
+                    Key(KeyEvent {
+                        code,
+                        kind: KeyEventKind::Press,
+                        ..
+                    }) if key_direction(code).is_some() => {
+                        self.direction = key_direction(code).unwrap()
+                    }
+                    event => self.unhandled_event = format!("Event: {:?}", event),
+                },
+
+                Message::Tick => {
+                    self.elapsed_ticks += 1;
+                    let head = self.line.last().unwrap();
+                    self.line.push((
+                        (head.0 as i32 + self.direction.0) as u16,
+                        (head.1 as i32 + self.direction.1) as u16,
+                    ));
+                }
+            }
         }
 
-        self.stdout.flush()?;
-
         Ok(())
+    }
+
+    fn spawn_periodic_timer(sender: Sender<Message>) {
+        thread::spawn(move || loop {
+            thread::sleep(Duration::from_millis(300));
+            if sender.send(Message::Tick).is_err() {
+                // no receiver (i.e. main thread has exited)
+                break;
+            }
+        });
+    }
+
+    fn spawn_event_receiver(sender: Sender<Message>) {
+        thread::spawn(move || loop {
+            let event = crossterm::event::read().expect("Receiving event");
+            if sender.send(Message::Event(event)).is_err() {
+                // no receiver (i.e. main thread has exited)
+                break;
+            }
+        });
     }
 
     fn draw_border(&mut self) -> Result<()> {
@@ -101,8 +161,35 @@ impl App {
 
 impl Drop for App {
     fn drop(&mut self) {
-        self.stdout.queue(LeaveAlternateScreen).unwrap();
-        self.stdout.flush().unwrap();
-        terminal::disable_raw_mode().unwrap();
+        restore_terminal(&mut self.stdout);
     }
+}
+
+enum Message {
+    Event(Event),
+    Tick,
+}
+
+fn key_direction(pressed_key_code: KeyCode) -> Option<(i32, i32)> {
+    return match pressed_key_code {
+        KeyCode::Left => Some((-1, 0)),
+        KeyCode::Right => Some((1, 0)),
+        KeyCode::Up => Some((0, -1)),
+        KeyCode::Down => Some((0, 1)),
+        _ => None,
+    };
+}
+
+fn claim_terminal(stdout: &mut Stdout) -> Result<()> {
+    terminal::enable_raw_mode()?;
+    stdout.queue(EnterAlternateScreen)?;
+    stdout.queue(Hide)?;
+    stdout.flush()?;
+    Ok(())
+}
+
+fn restore_terminal(stdout: &mut Stdout) {
+    stdout.queue(LeaveAlternateScreen).unwrap();
+    stdout.flush().unwrap();
+    terminal::disable_raw_mode().unwrap();
 }
